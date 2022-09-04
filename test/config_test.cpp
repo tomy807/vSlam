408f72f057a8bdcfd1aee36ed0f96aee7bfac20f
tomy807 <tomy807@naver.com>
2022-09-04 15:39:28 +0900

    Test for finding Directory of RGB and Depth And Fix g2o.cmake

--- test/config_test.cpp
+++ test/config_test.cpp
@@ -1,10 +1,33 @@
 #include <gtest/gtest.h>
 #include "../include/config.hpp"
+#include <fstream>
 #include <iostream>
+
+using namespace myslam;
 
 TEST(CONFIG,CONFIGTEST){
     string filedir="/home/tomy807/vslam/config/default.yaml";
-    myslam::Config::setParameterFile ( filedir);
-    string dataset_dir = myslam::Config::get<string> ( "dataset_dir" );
+    Config::setParameterFile ( filedir);
+    string dataset_dir = Config::get<string> ( "dataset_dir" );
     EXPECT_TRUE(dataset_dir=="/home/tomy807/rgbd_dataset_freiburg1_xyz");
+    ifstream fin ( dataset_dir+"/associations.txt" );
+    if ( !fin )
+    {
+        cout<<"please generate the associate file called associate.txt!"<<endl;
+    }
+    vector<string> rgb_files, depth_files;
+    vector<double> rgb_times, depth_times;
+    while ( !fin.eof() )
+    {
+        string rgb_time, rgb_file, depth_time, depth_file;
+        fin>>rgb_time>>rgb_file>>depth_time>>depth_file;
+        rgb_times.push_back ( atof ( rgb_time.c_str() ) );
+        depth_times.push_back ( atof ( depth_time.c_str() ) );
+        rgb_files.push_back ( dataset_dir+"/"+rgb_file );
+        depth_files.push_back ( dataset_dir+"/"+depth_file );
+
+        if ( fin.good() == false )
+            break;
+    }
+
 }
